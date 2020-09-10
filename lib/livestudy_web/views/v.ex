f5bbbe96427c9b01f1b93d1a89e39c134bb31207
raetam <raetamslog@gmail.com@gmail.com>
2020-09-10 11:49:47 +0900

    add::view_function::style_desc

--- lib/livestudy_web/views/v.ex
+++ lib/livestudy_web/views/v.ex
@@ -1,3 +1,12 @@
 defmodule LivestudyWeb.V do
   use LivestudyWeb, :view
+
+  def style_desc(w,h) do
+    "
+    display: grid ;
+      color: white ; width: #{w}px;height: #{h}px;
+      border: 1px solid #ccc;
+      border-radius: 4px; margin: 0px;
+     "
+  end
 end
